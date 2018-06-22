65cf545d85cd299d2974e20d4b610964359f7fd0
YuCejun <yucj@innosilicon.com.cn>
2018-06-22 10:14:36 +0800

    Only show average temperature in web

--- api.c
+++ api.c
@@ -2030,8 +2030,8 @@ static void ascstatus(struct io_data *io_data, int asc, bool isjson, bool precom
         root = api_add_string(root, "Enabled", enabled, false);
         root = api_add_string(root, "Status", status, false);
         root = api_add_temp(root, "Temperature", &temp, false);
-        root = api_add_temp(root, "TempMAX", &temp_max, false);
-        root = api_add_temp(root, "TempMIN", &temp_min, false);
+        //root = api_add_temp(root, "TempMAX", &temp_max, false);
+        //root = api_add_temp(root, "TempMIN", &temp_min, false);
         root = api_add_int(root, "CHIP", &(cgpu->chip_num), false);
         root = api_add_int(root, "CORE", &(cgpu->core_num), false);
         root = api_add_int(root, "DUTY", &(cgpu->fan_duty), false);
